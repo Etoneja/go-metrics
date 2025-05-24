870fe556f17226a40e3b7c226bf57293759d1e33
Mikhail Zanozin <m.zanozin@corp.mail.ru>
2025-05-25 00:34:16 +0300

    (agent) Implement agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"time"
+)
+
+func main() {
+	log.Println("Agent started")
+
+	log.Println("ServerEndpoint", defaultServerEndpoint)
+	log.Println("PollInterval", defaultPollInterval)
+	log.Println("ReportInterval", defaultReportInterval)
+
+	stats := newStats()
+
+	pollDuration := time.Second * time.Duration(defaultPollInterval)
+	poller := newPoller(stats, pollDuration)
+
+	reportDuration := time.Second * time.Duration(defaultReportInterval)
+	reporter := NewReporter(stats, defaultServerEndpoint, reportDuration)
+
+	go poller.runRoutine()
+	go reporter.runRoutine()
+
+	select {}
+}
