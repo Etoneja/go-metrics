9fe84298341f928bbaec62d5a2c09a2be8c7f1df
Mikhail Zanozin <m.zanozin@corp.mail.ru>
2025-05-27 18:09:56 +0300

    (server) Add new handlers

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"math"
 	"net/http"
+	"sort"
 	"strconv"
 
 	"github.com/go-chi/chi/v5"
@@ -46,4 +49,92 @@ func MetricUpdateHandler(store Storager) http.HandlerFunc {
 
 	}
 
+}
+
+func MetricGetHandler(store Storager) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		metricType := chi.URLParam(r, "metricType")
+		metricName := chi.URLParam(r, "metricName")
+
+		if metricType == metricTypeGauge {
+			value, ok, err := store.GetGauge(metricName)
+			if err != nil {
+				http.Error(w, "Internal Error", http.StatusInternalServerError)
+			}
+			if !ok {
+				http.Error(w, "Not Found", http.StatusNotFound)
+			}
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, "%v", value)
+			return
+		}
+
+		if metricType == metricTypeCounter {
+			value, ok, err := store.GetCounter(metricName)
+			if err != nil {
+				http.Error(w, "Internal Error", http.StatusInternalServerError)
+			}
+			if !ok {
+				http.Error(w, "Not Found", http.StatusNotFound)
+			}
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, "%v", value)
+			return
+		}
+
+		http.Error(w, "Not Found", http.StatusNotFound)
+
+	}
+}
+
+func MetricListHandler(store Storager) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		gauges, err := store.ListGauges()
+		if err != nil {
+			http.Error(w, "Internal Error", http.StatusInternalServerError)
+		}
+		counters, err := store.ListCounters()
+		if err != nil {
+			http.Error(w, "Internal Error", http.StatusInternalServerError)
+		}
+
+		gaugesKeys := make([]string, 0, len(gauges))
+		for k := range gauges {
+			gaugesKeys = append(gaugesKeys, k)
+		}
+
+		sort.Strings(gaugesKeys)
+
+		countersKeys := make([]string, 0, len(counters))
+		for k := range counters {
+			countersKeys = append(countersKeys, k)
+		}
+
+		sort.Strings(countersKeys)
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintln(w, "<html><body><pre>")
+
+		fmt.Fprintln(w, "<b>gauges</b>")
+		for _, k := range gaugesKeys {
+			v := gauges[k]
+			if v == math.Trunc(v) {
+				fmt.Fprintf(w, "%s=%.0f\n", k, v)
+			} else {
+				fmt.Fprintf(w, "%s=%.2f\n", k, v)
+			}
+		}
+
+		fmt.Fprintln(w, "\n<b>counters</b>")
+		for _, k := range countersKeys {
+			v := counters[k]
+			fmt.Fprintf(w, "%s=%d\n", k, v)
+		}
+
+		fmt.Fprintln(w, "</pre></body></html>")
+
+	}
 }
