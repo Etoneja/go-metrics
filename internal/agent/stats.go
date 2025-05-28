2bd4c837ddeff8956a443dfeadf58168b391c213
Mikhail Zanozin <m.zanozin@corp.mail.ru>
2025-05-28 16:18:27 +0300

    (agent) Minor fixes in stats/agent

--- internal/agent/stats.go
+++ internal/agent/stats.go
@@ -11,7 +11,7 @@ import (
 type metric struct {
 	kind  string
 	name  string
-	value string
+	value any
 }
 
 func newStats() *Stats {
@@ -48,38 +48,38 @@ func (s *Stats) dump() []metric {
 	defer s.mu.RUnlock()
 
 	metrics := []metric{
-		{kind: common.MetricTypeGauge, name: "Alloc", value: common.AnyToString(s.memStats.Alloc)},
-		{kind: common.MetricTypeGauge, name: "BuckHashSys", value: common.AnyToString(s.memStats.BuckHashSys)},
-		{kind: common.MetricTypeGauge, name: "Frees", value: common.AnyToString(s.memStats.Frees)},
-		{kind: common.MetricTypeGauge, name: "GCCPUFraction", value: common.AnyToString(s.memStats.GCCPUFraction)},
-		{kind: common.MetricTypeGauge, name: "GCSys", value: common.AnyToString(s.memStats.GCSys)},
-		{kind: common.MetricTypeGauge, name: "HeapAlloc", value: common.AnyToString(s.memStats.HeapAlloc)},
-		{kind: common.MetricTypeGauge, name: "HeapIdle", value: common.AnyToString(s.memStats.HeapIdle)},
-		{kind: common.MetricTypeGauge, name: "HeapInuse", value: common.AnyToString(s.memStats.HeapInuse)},
-		{kind: common.MetricTypeGauge, name: "HeapObjects", value: common.AnyToString(s.memStats.HeapObjects)},
-		{kind: common.MetricTypeGauge, name: "HeapReleased", value: common.AnyToString(s.memStats.HeapReleased)},
-		{kind: common.MetricTypeGauge, name: "HeapSys", value: common.AnyToString(s.memStats.HeapSys)},
-		{kind: common.MetricTypeGauge, name: "LastGC", value: common.AnyToString(s.memStats.LastGC)},
-		{kind: common.MetricTypeGauge, name: "Lookups", value: common.AnyToString(s.memStats.Lookups)},
-		{kind: common.MetricTypeGauge, name: "MCacheInuse", value: common.AnyToString(s.memStats.MCacheInuse)},
-		{kind: common.MetricTypeGauge, name: "MCacheSys", value: common.AnyToString(s.memStats.MCacheSys)},
-		{kind: common.MetricTypeGauge, name: "MSpanInuse", value: common.AnyToString(s.memStats.MSpanInuse)},
-		{kind: common.MetricTypeGauge, name: "MSpanSys", value: common.AnyToString(s.memStats.MSpanSys)},
-		{kind: common.MetricTypeGauge, name: "Mallocs", value: common.AnyToString(s.memStats.Mallocs)},
-		{kind: common.MetricTypeGauge, name: "NextGC", value: common.AnyToString(s.memStats.NextGC)},
-		{kind: common.MetricTypeGauge, name: "NumForcedGC", value: common.AnyToString(s.memStats.NumForcedGC)},
-		{kind: common.MetricTypeGauge, name: "NumGC", value: common.AnyToString(s.memStats.NumGC)},
-		{kind: common.MetricTypeGauge, name: "OtherSys", value: common.AnyToString(s.memStats.OtherSys)},
-		{kind: common.MetricTypeGauge, name: "PauseTotalNs", value: common.AnyToString(s.memStats.PauseTotalNs)},
-		{kind: common.MetricTypeGauge, name: "StackInuse", value: common.AnyToString(s.memStats.StackInuse)},
-		{kind: common.MetricTypeGauge, name: "StackSys", value: common.AnyToString(s.memStats.StackSys)},
-		{kind: common.MetricTypeGauge, name: "Sys", value: common.AnyToString(s.memStats.Sys)},
-		{kind: common.MetricTypeGauge, name: "TotalAlloc", value: common.AnyToString(s.memStats.TotalAlloc)},
+		{kind: common.MetricTypeGauge, name: "Alloc", value: s.memStats.Alloc},
+		{kind: common.MetricTypeGauge, name: "BuckHashSys", value: s.memStats.BuckHashSys},
+		{kind: common.MetricTypeGauge, name: "Frees", value: s.memStats.Frees},
+		{kind: common.MetricTypeGauge, name: "GCCPUFraction", value: s.memStats.GCCPUFraction},
+		{kind: common.MetricTypeGauge, name: "GCSys", value: s.memStats.GCSys},
+		{kind: common.MetricTypeGauge, name: "HeapAlloc", value: s.memStats.HeapAlloc},
+		{kind: common.MetricTypeGauge, name: "HeapIdle", value: s.memStats.HeapIdle},
+		{kind: common.MetricTypeGauge, name: "HeapInuse", value: s.memStats.HeapInuse},
+		{kind: common.MetricTypeGauge, name: "HeapObjects", value: s.memStats.HeapObjects},
+		{kind: common.MetricTypeGauge, name: "HeapReleased", value: s.memStats.HeapReleased},
+		{kind: common.MetricTypeGauge, name: "HeapSys", value: s.memStats.HeapSys},
+		{kind: common.MetricTypeGauge, name: "LastGC", value: s.memStats.LastGC},
+		{kind: common.MetricTypeGauge, name: "Lookups", value: s.memStats.Lookups},
+		{kind: common.MetricTypeGauge, name: "MCacheInuse", value: s.memStats.MCacheInuse},
+		{kind: common.MetricTypeGauge, name: "MCacheSys", value: s.memStats.MCacheSys},
+		{kind: common.MetricTypeGauge, name: "MSpanInuse", value: s.memStats.MSpanInuse},
+		{kind: common.MetricTypeGauge, name: "MSpanSys", value: s.memStats.MSpanSys},
+		{kind: common.MetricTypeGauge, name: "Mallocs", value: s.memStats.Mallocs},
+		{kind: common.MetricTypeGauge, name: "NextGC", value: s.memStats.NextGC},
+		{kind: common.MetricTypeGauge, name: "NumForcedGC", value: s.memStats.NumForcedGC},
+		{kind: common.MetricTypeGauge, name: "NumGC", value: s.memStats.NumGC},
+		{kind: common.MetricTypeGauge, name: "OtherSys", value: s.memStats.OtherSys},
+		{kind: common.MetricTypeGauge, name: "PauseTotalNs", value: s.memStats.PauseTotalNs},
+		{kind: common.MetricTypeGauge, name: "StackInuse", value: s.memStats.StackInuse},
+		{kind: common.MetricTypeGauge, name: "StackSys", value: s.memStats.StackSys},
+		{kind: common.MetricTypeGauge, name: "Sys", value: s.memStats.Sys},
+		{kind: common.MetricTypeGauge, name: "TotalAlloc", value: s.memStats.TotalAlloc},
 
-		{kind: common.MetricTypeGauge, name: "RandomValue", value: common.AnyToString(s.RandomValue)},
+		{kind: common.MetricTypeGauge, name: "RandomValue", value: s.RandomValue},
 
 		// NOTE: Should we send global counter of diff since last report?
-		{kind: common.MetricTypeCounter, name: "PollCount", value: common.AnyToString(s.PollCount)},
+		{kind: common.MetricTypeCounter, name: "PollCount", value: s.PollCount},
 	}
 	return metrics
 }
