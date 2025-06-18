7b428c5b4b666af868c719e42c505eed6bd88e0e
Mikhail Zanozin <m.zanozin@corp.mail.ru>
2025-06-18 17:44:18 +0300

    (server) Split middlewares

--- internal/server/gzip_middleware.go
+++ internal/server/gzip_middleware.go
@@ -4,51 +4,9 @@ import (
 	"bytes"
 	"compress/gzip"
 	"log"
-	"mime"
 	"net/http"
 	"strings"
-	"time"
-
-	"go.uber.org/zap"
 )
-
-type responseData struct {
-	status int
-	size   int
-}
-
-type loggingResponseWriter struct {
-	http.ResponseWriter
-	responseData *responseData
-}
-
-func (r *loggingResponseWriter) Write(b []byte) (int, error) {
-	size, err := r.ResponseWriter.Write(b)
-	r.responseData.size += size
-	return size, err
-}
-
-func (r *loggingResponseWriter) WriteHeader(statusCode int) {
-	r.ResponseWriter.WriteHeader(statusCode)
-	r.responseData.status = statusCode
-}
-
-var compressibleTypes = map[string]bool{
-	"application/json": true,
-	"text/html":        true,
-}
-
-func isCompressibleContentType(contentType string) bool {
-	mimeType, _, err := mime.ParseMediaType(contentType)
-	if err != nil {
-		return false
-	}
-	res, ok := compressibleTypes[mimeType]
-	if !ok {
-		res = false
-	}
-	return res
-}
 
 type bufferedResponseWriter struct {
 	http.ResponseWriter
@@ -90,37 +48,6 @@ func (w *bufferedResponseWriter) Flush() {
 	w.writeHeaders()
 	if w.buf.Len() > 0 {
 		w.ResponseWriter.Write(w.buf.Bytes())
-	}
-}
-
-func LoggerMiddleware(logger *zap.Logger) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		logFn := func(w http.ResponseWriter, r *http.Request) {
-			start := time.Now()
-
-			responseData := &responseData{
-				status: 0,
-				size:   0,
-			}
-
-			lw := &loggingResponseWriter{
-				ResponseWriter: w,
-				responseData:   responseData,
-			}
-
-			next.ServeHTTP(lw, r)
-
-			logger.Info("Request processed",
-				zap.String("uri", r.RequestURI),
-				zap.String("method", r.Method),
-				zap.Duration("duration", time.Since(start)),
-				zap.Int("size", responseData.size),
-				zap.Int("statusCode", responseData.status),
-			)
-
-		}
-
-		return http.HandlerFunc(logFn)
 	}
 }
 
