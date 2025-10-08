2b08e827c366c61cef2b9d5df6094b7e93e34d15
Mikhail Zanozin <m.zanozin@corp.mail.ru>
2025-10-09 01:22:41 +0300

    (all) Allow load config from json file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -17,14 +17,12 @@ import (
 func main() {
 	version.Print()
 
-	cfg := server.PrepareConfig()
-
 	logger.Init(false)
-	defer func() {
-		if err := logger.Sync(); err != nil {
-			logger.Get().Warn("failed to sync logger", zap.Error(err))
-		}
-	}()
+
+	cfg, err := server.PrepareConfig()
+	if err != nil {
+		logger.Get().Fatal("Failed prepare config", zap.Error(err))
+	}
 
 	privateKey, err := common.LoadPrivateKey(cfg.CryptoKey)
 	if err != nil {
@@ -41,6 +39,8 @@ func main() {
 		zap.Uint("StoreInterval", cfg.StoreInterval),
 		zap.String("FileStoragePath", cfg.FileStoragePath),
 		zap.Bool("Restore", cfg.Restore),
+		zap.String("CryptoKey", cfg.CryptoKey),
+		zap.String("ConfigFile", cfg.ConfigFile),
 	)
 
 	router := server.NewRouter(store, cfg.HashKey, privateKey)
