3cd5f084cde6e36bdddd4fdfeec6698aee4a560d
Mikhail Zanozin <m.zanozin@corp.mail.ru>
2025-10-24 02:35:26 +0300

    (server) Split StartGRPCServer into functions

--- internal/server/grpc_server.go
+++ internal/server/grpc_server.go
@@ -9,31 +9,45 @@ import (
 	"google.golang.org/grpc"
 )
 
-func StartGRPCServer(store Storager, logger *zap.Logger, cfg *config, serverErrChan chan<- error) (*grpc.Server, error) {
-	grpcServer := grpc.NewServer(
+func createGRPCServer(logger *zap.Logger, cfg *config) *grpc.Server {
+	return grpc.NewServer(
 		grpc.ChainUnaryInterceptor(
 			loggingInterceptor(logger),
 			TrustedSubnetInterceptor(cfg.TrustedSubnet, logger),
 		),
 	)
+}
 
+func registerServices(server *grpc.Server, store Storager, logger *zap.Logger) {
 	grpcMetricsServer := NewGRPCServer(store, logger)
+	proto.RegisterMetricsServiceServer(server, grpcMetricsServer)
+}
 
-	proto.RegisterMetricsServiceServer(grpcServer, grpcMetricsServer)
-
-	listener, err := net.Listen("tcp", cfg.ServerGRPCAddress)
+func startServing(server *grpc.Server, addr string, logger *zap.Logger, serverErrChan chan<- error) error {
+	listener, err := net.Listen("tcp", addr)
 	if err != nil {
-		return nil, fmt.Errorf("failed to listen gRPC on %s: %w", cfg.ServerGRPCAddress, err)
+		return fmt.Errorf("failed to listen gRPC on %s: %w", addr, err)
 	}
 
 	go func() {
-		logger.Info("gRPC server starting", zap.String("addr", cfg.ServerGRPCAddress))
-		err := grpcServer.Serve(listener)
+		logger.Info("gRPC server starting", zap.String("addr", addr))
+		err := server.Serve(listener)
 		if err != nil && err != grpc.ErrServerStopped {
 			logger.Error("gRPC server failed", zap.Error(err))
 			serverErrChan <- err
 		}
 	}()
+
+	return nil
+}
+
+func StartGRPCServer(store Storager, logger *zap.Logger, cfg *config, serverErrChan chan<- error) (*grpc.Server, error) {
+	grpcServer := createGRPCServer(logger, cfg)
+	registerServices(grpcServer, store, logger)
+
+	if err := startServing(grpcServer, cfg.ServerGRPCAddress, logger, serverErrChan); err != nil {
+		return nil, err
+	}
 
 	return grpcServer, nil
 }
